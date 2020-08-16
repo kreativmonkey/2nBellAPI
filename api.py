361f9e5ba45655fbd39faa980f2c31629d304062
Kreativmonkey <kreativmonkey@calyrium.org>
2020-08-16 23:12:42 +0200

    linting and fix some syntax errors

--- api.py
+++ api.py
@@ -37,47 +37,47 @@ class BellAPIWrapper():
     self._session   = None
     
     self._username  = username
-		self._password  = password
-		self._port      = port
-		self._host      = host
+    self._password  = password
+    self._port      = port
+    self._host      = host
     self._payload   = []
     
 		# Setting the session URL and initialize sessionID
-		self._sessionUrl  = 'https://' + host + '/ajax?sid='
+    self._sessionUrl  = 'https://' + host + '/ajax?sid='
     self._sessionID   = ''
     
     # Start the session by first Login
-		self._login()
+    self._login()
     
-	''' Login and start the session '''
+  ''' Login and start the session '''
   def _login(self):
     # TODO: Caching Session: https://stackoverflow.com/questions/12737740/python-requests-and-persistent-sessions
  
-		self._session = requests.Session()
-		# Workaround for self signed certificates, need a better solution!
+    self._session = requests.Session()
+    # Workaround for self signed certificates, need a better solution!
     # TODO: support selfe signed certificate
-		self._session.verify = False
-				
+    self._session.verify = False
+        
     resp = self._send('[{"command":"system.login","user":"'+ self._username +'","password":"'+ self._password +'"}]', False)
-    
-		# Save the session ID
-		self._sessionID = json.loads(resp.text)[0]["sid"]
-    
-		return resp
+
+    # Save the session ID
+    self._sessionID = json.loads(resp.text)[0]["sid"]
+
+    return resp
   
   ''' Logout and close the session '''
   def _logout(self):
-		resp = self._send('[{"command":"system.logout"}]', False)
-		
+    resp = self._send('[{"command":"system.logout"}]', False)
+
     # If logout is ok close the session, i need a better solution for error handling in this case.
-		if json.loads(resp.text)[0]['status'] == 'ok':
+    if json.loads(resp.text)[0]['status'] == 'ok':
       self._sessionID = ''
-			self._session.close()
-		
+      self._session.close()
+
     return resp
   
   ''' Validate if a string is a json '''
-  def _is_json(jsonstring):
+  def _is_json(self, jsonstring):
     try:
       json_object = json.loads(jsonstring)
     except ValueError as e:
@@ -85,7 +85,7 @@ class BellAPIWrapper():
     return True
     
   ''' Sending requests to the 2N Bell and getting the response '''
-	def _send(self, p, checkSession = True):
+  def _send(self, p, checkSession = True):
     # TODO: Check if the session is already opened
     if checkSession:
       # First Workaround
@@ -94,20 +94,20 @@ class BellAPIWrapper():
          
     # check if payload is already a valid json
     if self._is_json(p):
-      pyload = p
+      payload = p
       
     # convert a list of dict into JSON
     if isinstance(p, list):
       payload = json.dumps(p)
       
     # return des the response
-		return self._session.post(self._sessionUrl + self._sessionID, data=payload)
+    return self._session.post(self._sessionUrl + self._sessionID, data=payload)
 
   ''' Create the payload to set values '''
-  def _payloadSet(self, path, value)
+  def _payloadSet(self, path, value):
     # Check if path already in the payload
-    #if any(p['path'] == path for p in self._payload):
-    #  raise ValueError('Path already in the Payload')
+    # if any(p['path'] == path for p in self._payload):
+    # raise ValueError('Path already in the Payload')
     
     # Update if path already in the payload
     for p in self._payload:
@@ -117,7 +117,7 @@ class BellAPIWrapper():
         return 
     
     # TODO: Validate value for path
-      
+    payload = {}
     payload["command"] = "db.set"
     payload["path"] = SET_TOPICS[path]
     payload["value"] = value
@@ -125,11 +125,12 @@ class BellAPIWrapper():
     return payload
     
   ''' Create the payload to get states '''
-  def _payloadGet(self, path)
+  def _payloadGet(self, path):
     # Check if path already in the payload
     if any(p['path'] == path for p in self._payload):
       raise ValueError('Path already in the Payload')
     
+    payload = {}
     payload["command"] = "db.get"
     payload["path"] = STATE_TOPICS[path]
     
@@ -149,7 +150,7 @@ class BellAPIWrapper():
         
       payload = self._payloadGet(path)
         
-		elif command == "db.set":
+    elif command == "db.set":
       if path not in SET_TOPICS:
         raise ValueError(path + ' is not supported ore has no get function')
       # Check if the Value is set on command "db.set"
@@ -163,7 +164,7 @@ class BellAPIWrapper():
   
   ''' Send the generated payload '''
   def sendPayloads(self):
-    if not self._send(payload):
+    if not self._send(self._payload):
       return 1
   
     # after success delet saved payload
@@ -174,7 +175,7 @@ class BellAPIWrapper():
   ''' Set the brightness of the key light '''
   def setBrightness(self, value):
     # Check the correct value
-		if not value >= 0 and not value <= 100:
+    if not value >= 0 and not value <= 100:
       return
       
     payload = []
@@ -191,80 +192,80 @@ class BellAPIWrapper():
       payload.append(self._payloadGet(state))
 
     self._send(payload)
-    				
-		return 
+
+    return 
     
     
 class TwoN():
-	
-	def __init__(
+    
+  def __init__(
         self,
         password,
         host,
         username = DEFAULT_USERNAME,
         port=DEFAULT_PORT,
     ):
-		self.s = None
-		
-		self.username = username
-		self.password = password
-		self.port = port
-		self.host = host
-		# Setting the session URL to the default
-		self.sessionUrl = 'https://' + host + '/ajax?sid='
-		
-		self.login(username, password)
-		
-		self.State = self.TwoNState()
-		
-	def getState(self):
-		return self.State
-		
-	def login(self, username, password):
-		self.s = requests.Session()
-		# Workaround for self signed certificates, need a better solution!
-		self.s.verify = False
-		
-		# Payload for login
-		login_payload = '[{"command":"system.login","user":"'+username+'","password":"'+password+'"}]'
-		resp = self.sendRequest(login_payload)
-		
-		# Adding the session ID to the session URL
-		self.sessionUrl += json.loads(resp.text)[0]["sid"]
-		return resp
+    self.s = None
+
+    self.username = username
+    self.password = password
+    self.port = port
+    self.host = host
+    # Setting the session URL to the default
+    self.sessionUrl = 'https://' + host + '/ajax?sid='
+
+    self.login(username, password)
+
+    self.State = self.TwoNState()
+    
+  def getState(self):
+    return self.State
+    
+  def login(self, username, password):
+    self.s = requests.Session()
+    # Workaround for self signed certificates, need a better solution!
+    self.s.verify = False
+    
+    # Payload for login
+    login_payload = '[{"command":"system.login","user":"'+username+'","password":"'+password+'"}]'
+    resp = self.sendRequest(login_payload)
+    
+    # Adding the session ID to the session URL
+    self.sessionUrl += json.loads(resp.text)[0]["sid"]
+    return resp
         
-	def logout(self):
-		resp = json.loads(self.s.post(self.sessionUrl, data='[{"command":"system.logout"}]').text)[0]['status']
-		# If logout is ok close the session, i need a better solution for error handling in this case.
-		if resp == 'ok':
-			self.s.close()
-		return resp
-		
-	def TwoNState(self):
-		payload = self.createPayload('db.get', STATE_TOPICS)
-		response = self.sendRequest(payload)
-				
-		return self.generateJsonFromRequest(STATE_TOPICS, response)
-		
-	def generateJsonFromRequest(self, request, response):
-		states = {}
-		for i, req in enumerate(request, start=0):
-			states[req] = json.loads(response.text)[i]['value']
-		
-		return states
-			
-	def sendRequest(self, payload):
-		return self.s.post(self.sessionUrl, data=payload)
-	
-	def createPayload(self, command, path):
-		payload = '['
-		for p in path.values():
-			payload += '{"command":"'+ command +'", "path":"' + p + '"},'
-		
-		# Remove the last ',' from the Payload and add ]
-		payload = payload.rstrip(',') + ']'
-		
-		return payload
+  def logout(self):
+    resp = json.loads(self.s.post(self.sessionUrl, data='[{"command":"system.logout"}]').text)[0]['status']
+    # If logout is ok close the session, i need a better solution for error handling in this case.
+    if resp == 'ok':
+      self.s.close()
+    return resp
+    
+  def TwoNState(self):
+    payload = self.createPayload('db.get', STATE_TOPICS)
+    response = self.sendRequest(payload)
+        
+    return self.generateJsonFromRequest(STATE_TOPICS, response)
+    
+  def generateJsonFromRequest(self, request, response):
+    states = {}
+    for i, req in enumerate(request, start=0):
+      states[req] = json.loads(response.text)[i]['value']
+    
+    return states
+      
+  def sendRequest(self, payload):
+    return self.s.post(self.sessionUrl, data=payload)
+
+  def createPayload(self, command, path):
+    payload = '['
+    for p in path.values():
+      payload += '{"command":"'+ command +'", "path":"' + p + '"},'
+    
+    # Remove the last ',' from the Payload and add ]
+    payload = payload.rstrip(',') + ']'
+    
+    return payload
 
 
 host = '192.168.5.5'
